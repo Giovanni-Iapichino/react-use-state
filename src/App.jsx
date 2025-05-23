f4b8c2f748300c51c9aafcf9bb94a0f9ef83b339
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-23 16:09:20 +0200

    add-state

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Header from "./components/Header";
 
 // DATA COMPONENTS
 import languages from "./data/languages";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedLang, setSelectedLang] = useState(languages[0]);
   return (
     <>
       <div className="container">
@@ -14,21 +16,21 @@ export default function App() {
       </div>
       <div className="container">
         <div className="d-flex gap-4 p-3 mt-5">
-          <button className="btn btn-primary">HTML</button>
-          <button className="btn btn-primary">CSS</button>
-          <button className="btn btn-primary">JavaScript</button>
-          <button className="btn btn-primary">Node.js</button>
-          <button className="btn btn-primary">Express</button>
-          <button className="btn btn-primary">ReactJS</button>
+          {languages.map((lang) => (
+            <button
+              key={lang.id}
+              className={`btn btn-primary`}
+              onClick={() => setSelectedLang(lang)}
+            >
+              {lang.title}
+            </button>
+          ))}
         </div>
 
         <div class="card ms-3 mt-3">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card’s content.
-            </p>
+            <h5 class="card-title">{selectedLang.title}</h5>
+            <p class="card-text">{selectedLang.description}</p>
           </div>
         </div>
       </div>
